655a2fcab0ae4da8e6aeee43d7df1e2d38908366
Julieta <jfllaz@outlook.com>
2020-08-21 19:41:33 -0500

    Esqueletos de clases creadas con sus respectivos metodos

---
+++ src/javabowling/Frames.java
@@ -0,0 +1,16 @@
+
+package javabowling;
+
+
+public class Frames {
+     int frame [] = new int [9];
+        int tiro;
+        int primerTiro;
+        int siguienteTiro;
+        int puntos;
+        int puntajeTotal;
+        int puntaje = puntos + puntajeTotal;
+        
+        
+        
+}
